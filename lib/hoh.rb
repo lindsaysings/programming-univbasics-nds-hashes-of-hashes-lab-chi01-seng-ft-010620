747bab6aa82a374b9dac8f4d8e2ec73abcf75787
Lindsay M <lindsaymecher@gmail.com>
2019-12-30 20:28:13 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,51 +4,48 @@
 #
 # The tests will guide your construction.
 #
-=begin
- PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-   label: "Phylum",
-     sub_category: {
-       label: "Class",
-       sub_category: {
-         label: "Order",
-         sub_category: {
-           label: "Family",
-           sub_category: {
-             label: "Genus",
-             sub_category: {
-               label: "Species",
-               sub_category: {
+
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#   label: "Phylum",
+#     sub_category: {
+#       label: "Class",
+#       sub_category: {
+#         label: "Order",
+#         sub_category: {
+#           label: "Family",
+#           sub_category: {
+#             label: "Genus",
+#             sub_category: {
+#               label: "Species",
+#               sub_category: {
                  
-               }
-             }
-           }
-         }
-       }
-     }
-   }
- }
-# 
- PORTION_2 = {
-   label: "Order"
- }
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+# # 
+# PORTION_2 = {
+#   label: "Order"
+# }
  
-# 
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
-# 
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
- 
- =end
- 
+# # 
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
+# # 
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
 {
