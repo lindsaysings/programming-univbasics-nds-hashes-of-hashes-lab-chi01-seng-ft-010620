7ce2159fc2a5969a9339d5415d8bf25da4f8c61c
Lindsay M <lindsaymecher@gmail.com>
2019-12-30 20:23:43 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -9,7 +9,22 @@
    sub_category: {
    label: "Phylum",
      sub_category: {
-       label: "Class"
+       label: "Class",
+       sub_category: {
+         label: "Order",
+         sub_category: {
+           label: "Family",
+           sub_category: {
+             label: "Genus",
+             sub_category: {
+               label: "Species",
+               sub_category: {
+                 label: 
+               }
+             }
+           }
+         }
+       }
      }
    }
  }
